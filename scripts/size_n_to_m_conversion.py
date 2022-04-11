a830927964df42a78e67b5b5fb11949b85916084
Adam Dama <dama2@illinois.edu>
2022-04-11 01:58:13 -0500

    n to m conversion script

---
+++ scripts/size_n_to_m_conversion.py
@@ -0,0 +1,58 @@
+import sys
+
+import pandas as pd
+import numpy as np
+
+from ..global_vars import BASE_INGREDIENTS
+from ..utils import combined_round_data
+
+
+def import_data(experiment_path, max_n=None):
+    data = combined_round_data(experiment_path, max_n=max_n, sort=False)
+    data = data.drop(
+        columns=[
+            "direction",
+            "is_redo",
+            "parent_plate",
+            "initial_od",
+            "final_od",
+            "bad",
+            "delta_od",
+        ]
+    )
+    data = data.rename(columns={"var": "growth_pred_var", "depth": "num_aa_removed"})
+    return data
+
+
+def fill_new_ingredients(data, old_size, new_ingredients, fill_on_right=True):
+    n_new = len(new_ingredients)
+    fill_df = pd.DataFrame(
+        np.ones((len(data), n_new), dtype=int), columns=new_ingredients
+    )
+
+    if fill_on_right:
+        left = data.iloc[:, :old_size]
+        right = data.iloc[:, old_size:]
+        center = fill_df
+        dfs = (left, center, right)
+        col_names = list(left.columns) + new_ingredients + list(right.columns)
+    else:
+        dfs = (fill_df, data)
+        col_names = new_ingredients + list(data.columns)
+
+    data = pd.concat(dfs, axis=1, ignore_index=True)
+    data.columns = col_names
+    return data
+
+
+if __name__ == "__main__":
+    path = "../experiments/08-20-2021_12"
+
+    data = import_data(path)
+    new_ingredients = BASE_INGREDIENTS
+    data_filled = fill_new_ingredients(data, 20, new_ingredients, fill_on_right=True)
+
+    # data_filled_2 = fill_new_ingredients(data, 20, new_ingredients, fill_on_right=False)
+
+    print(data_filled.columns)
+    print(data_filled)
